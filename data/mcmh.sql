dd0c9e6727744d4ef726fa131d4fd37bb9b6c85d
baimoo <5716847+baimoo@user.noreply.gitee.com>
2020-04-25 01:17:44 +0800

    1.数据库结构修改 2.增加部分数据 3.更新数据库设计表格文件

--- data/mcmh.sql
+++ data/mcmh.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50628
 File Encoding         : 65001
 
-Date: 2020-04-23 22:43:00
+Date: 2020-04-25 01:15:06
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,18 +20,23 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `building`;
 CREATE TABLE `building` (
-  `bdid` int(11) NOT NULL,
+  `bdid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `baid` int(11) NOT NULL,
   `remark` varchar(255) NOT NULL,
   PRIMARY KEY (`bdid`),
   KEY `building_buildingAdmin_baid` (`baid`),
   CONSTRAINT `building_buildingAdmin_baid` FOREIGN KEY (`baid`) REFERENCES `buildingadmin` (`baid`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of building
 -- ----------------------------
+INSERT INTO `building` VALUES ('1', 'A栋', '1', '靠近操场，开运动会时会被吵醒');
+INSERT INTO `building` VALUES ('2', 'B栋', '2', '靠近校门，出校方便，有活动时一样吵闹');
+INSERT INTO `building` VALUES ('3', 'C栋', '3', '接近食堂，当心长胖');
+INSERT INTO `building` VALUES ('4', 'D栋', '4', '接近快递点，建议“富家子弟”居住');
+INSERT INTO `building` VALUES ('5', 'E栋', '5', '靠近校后门，嗯，你懂的');
 
 -- ----------------------------
 -- Table structure for buildingadmin
@@ -45,11 +50,16 @@ CREATE TABLE `buildingadmin` (
   `sex` int(11) NOT NULL,
   `phone` varchar(11) NOT NULL,
   PRIMARY KEY (`baid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of buildingadmin
 -- ----------------------------
+INSERT INTO `buildingadmin` VALUES ('1', 'QQ老冰', 'lilang', '123', '1', '18888888888');
+INSERT INTO `buildingadmin` VALUES ('2', '白墨', 'baimo', '123', '1', '18888888888');
+INSERT INTO `buildingadmin` VALUES ('3', 'QQ大冰', 'baofuqi', '123', '1', '15555555555');
+INSERT INTO `buildingadmin` VALUES ('4', 'QQ巨冰', 'chenfeng', '123', '0', '16666666666');
+INSERT INTO `buildingadmin` VALUES ('5', 'QQ小冰', 'yangxiaojun', '123', '1', '17777777777');
 
 -- ----------------------------
 -- Table structure for late
@@ -60,9 +70,12 @@ CREATE TABLE `late` (
   `date` date NOT NULL,
   `remark` varchar(255) NOT NULL,
   `sid` varchar(255) NOT NULL,
+  `baid` int(11) NOT NULL,
   PRIMARY KEY (`lid`),
-  KEY `late` (`sid`),
-  CONSTRAINT `late` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `late_student_sid` (`sid`),
+  KEY `late_buildingadmin_baid` (`baid`),
+  CONSTRAINT `late_buildingadmin_baid` FOREIGN KEY (`baid`) REFERENCES `buildingadmin` (`baid`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `late_student_sid` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -105,11 +118,31 @@ CREATE TABLE `room` (
   UNIQUE KEY `u_code` (`code`),
   KEY `room_building_bdid` (`bdid`),
   CONSTRAINT `room_building_bdid` FOREIGN KEY (`bdid`) REFERENCES `building` (`bdid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of room
 -- ----------------------------
+INSERT INTO `room` VALUES ('1', 'A1001', '1', '4', '15807472192', '四人间');
+INSERT INTO `room` VALUES ('2', 'A1002', '1', '4', '15606198839', '四人间');
+INSERT INTO `room` VALUES ('3', 'A1003', '1', '4', '13604422899', '四人间');
+INSERT INTO `room` VALUES ('4', 'A1004', '1', '4', '15107727635', '四人间');
+INSERT INTO `room` VALUES ('5', 'B1001', '2', '4', '15501314543', '四人间');
+INSERT INTO `room` VALUES ('6', 'B1002', '2', '4', '15707315995', '四人间');
+INSERT INTO `room` VALUES ('7', 'B1003', '2', '4', '15305720673', '四人间');
+INSERT INTO `room` VALUES ('8', 'B1004', '2', '4', '15704470563', '四人间');
+INSERT INTO `room` VALUES ('9', 'C1001', '3', '4', '13408431838', '四人间');
+INSERT INTO `room` VALUES ('10', 'C1002', '3', '4', '15008419041', '四人间');
+INSERT INTO `room` VALUES ('11', 'C1003', '3', '4', '13605062762', '四人间');
+INSERT INTO `room` VALUES ('12', 'C1004', '3', '4', '13603324866', '四人间');
+INSERT INTO `room` VALUES ('13', 'D1001', '4', '4', '13003863705', '四人间');
+INSERT INTO `room` VALUES ('14', 'D1002', '4', '4', '13708160322', '四人间');
+INSERT INTO `room` VALUES ('15', 'D1003', '4', '4', '15303473851', '四人间');
+INSERT INTO `room` VALUES ('16', 'D1004', '4', '4', '13408855266', '四人间');
+INSERT INTO `room` VALUES ('17', 'E1001', '5', '4', '13706624513', '四人间');
+INSERT INTO `room` VALUES ('18', 'E1002', '5', '4', '13603846149', '四人间');
+INSERT INTO `room` VALUES ('19', 'E1003', '5', '4', '15803022596', '四人间');
+INSERT INTO `room` VALUES ('20', 'E1004', '5', '4', '13903982479', '四人间');
 
 -- ----------------------------
 -- Table structure for student
@@ -143,8 +176,13 @@ CREATE TABLE `systemadmin` (
   `pwd` varchar(20) NOT NULL,
   `sex` int(11) DEFAULT NULL,
   PRIMARY KEY (`said`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of systemadmin
 -- ----------------------------
+INSERT INTO `systemadmin` VALUES ('1', '白墨', 'baimo', '123', '1');
+INSERT INTO `systemadmin` VALUES ('2', 'QQ大冰', 'baofuqi', '123', '1');
+INSERT INTO `systemadmin` VALUES ('3', 'QQ巨冰', 'chenfeng', '123', '0');
+INSERT INTO `systemadmin` VALUES ('4', 'QQ小冰', 'yangxiaojun', '123', '1');
+INSERT INTO `systemadmin` VALUES ('5', 'QQ老冰', 'lilang', '123', '1');
